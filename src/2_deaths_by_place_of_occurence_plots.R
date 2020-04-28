a5bb5d1a967b4527cb127b9017099a873ebefadf
Emma Vestesson <emma.vestesson@gmail.com>
2020-04-28 12:45:25 +0200

    Adde line chart with fewer categories

--- src/2_deaths_by_place_of_occurence_plots.R
+++ src/2_deaths_by_place_of_occurence_plots.R
@@ -8,7 +8,7 @@ date_breaks <- unique(EW$week_ended)
 
 ggplot(EW) + geom_col(aes(x=week_ended, y=total_deaths, fill=location)) +
   scale_x_date(breaks=date_breaks, date_labels = "%d %b %Y") +
-  scale_y_continuous(labels = scales::comma, limit=c(0,20000)) + 
+  scale_y_continuous(labels = scales::comma, limit=c(0,NA)) + 
   scale_fill_THF(breaks=legend_breaks, label=legend_labels, 
                  guide_legend(nrow=1, ncol=)) +
   theme_THF() +
@@ -26,13 +26,13 @@ ggplot(EW) + geom_col(aes(x=week_ended, y=total_deaths, fill=location)) +
 
 ggsave(here::here('output', 'deaths_by_place_of_occurence_bar_chart.pdf'))
 
-# plot
+# line plot
 ggplot(EW) + 
   geom_line(aes(x=week_ended, y=proportion_0_base, colour=location, group=location)) + 
   geom_point(aes(x=week_ended, y=proportion_0_base, colour=location), size=2, fill='white') +
   geom_point(aes(x=week_ended, y=proportion_0_base), color='white', size=1) + 
   scale_x_date(breaks=date_breaks, date_labels = "%d %b %Y") +
-  scale_y_continuous(labels = scales::percent_format(scale=1), limits = c(-60,100)) + 
+  scale_y_continuous(labels = scales::percent_format(scale=1), limits = c(-60,NA)) + 
   scale_colour_THF(breaks=legend_breaks, label=legend_labels) +
   theme_THF() +
   theme(plot.title = element_text(size=11, hjust = 0), 
@@ -44,7 +44,7 @@ ggplot(EW) +
         axis.text.y = element_text(angle = 0, vjust = 0, hjust = 0, face = 'plain'),
         plot.subtitle = element_text(size = 8)) + 
   labs(caption='Source: ONS', 
-       title = 'Deaths from any cause in care homes have increased by 99% since \nthe start of the COVID-19 outbreak',
+       title = 'Deaths from any cause in care homes have increased by 196% since \nthe start of the COVID-19 outbreak',
        subtitle = 'Percentage change in deaths from any cause by place of death in England and Wales, \nrelative to the week of 13 March 2020',
        y='Percentage change', x='')
 
@@ -53,6 +53,34 @@ ggsave(here::here('output', 'deaths_by_place_of_occurence.pdf'))
 
 # Save wide data
 EW_wide <- EW %>% 
-  select( week_ended, location, proportion) %>% 
-  pivot_wider(id_cols = location, names_from = week_ended, values_from = proportion)
+  select( week_ended, location, proportion_0_base) %>% 
+  pivot_wider(id_cols = location, names_from = week_ended, values_from = proportion_0_base)
 write_csv(EW_wide, here::here('data', 'England_Wales_place_of_death.csv') )
+
+# 
+EW$location_4groups <- factor(EW$location_4groups, levels=c("Care home", "Home", "Hospital (acute or community, not psychiatric)", "Other*"))
+# line plot with fewer categories
+ggplot(EW) + 
+  geom_line(aes(x=week_ended, y=proportion_0_base_4groups, colour=location_4groups, group=location_4groups)) + 
+  geom_point(aes(x=week_ended, y=proportion_0_base_4groups, colour=location_4groups), size=2, fill='white') +
+  geom_point(aes(x=week_ended, y=proportion_0_base_4groups), color='white', size=1) + 
+  scale_x_date(breaks=date_breaks, date_labels = "%d %b %Y") +
+  scale_y_continuous(labels = scales::percent_format(scale=1), limits = c(-60,NA)) + 
+  scale_colour_THF(breaks=legend_breaks, label=legend_labels) +
+  theme_THF() +
+  theme(plot.title = element_text(size=11, hjust = 0), 
+        plot.title.position='plot',
+        plot.caption = element_text(face = 'plain'),
+        legend.margin = margin(b = 10, l = -10, unit = 'mm'),
+        legend.justification = c("right", "top"),
+        axis.title.y = element_text(face = 'plain', size=8),
+        axis.text.x = element_text(angle = 0, vjust = 1, hjust = 0.5, face = 'plain'),
+        axis.text.y = element_text(angle = 0, vjust = 0, hjust = 0, face = 'plain'),
+        plot.subtitle = element_text(size = 8)) + 
+  labs(caption='Source: ONS \n*hospice, other communal establishment, elsewhere', 
+       title = 'Deaths from any cause in care homes have increased by 196% since \nthe start of the COVID-19 outbreak',
+       subtitle = 'Percentage change in deaths from any cause by place of death in England and Wales, \nrelative to the week of 13 March 2020',
+       y='Percentage change', x='')
+
+
+ggsave(here::here('output', 'deaths_by_place_of_occurence_4groups.pdf'))
