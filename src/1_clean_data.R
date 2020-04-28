6b125ac56ff200877ce6a0f7a1c43a330b0232e0
Emma Vestesson <emma.vestesson@gmail.com>
2020-04-28 12:44:48 +0200

    Updated with daily deaths

--- src/1_clean_data.R
+++ src/1_clean_data.R
@@ -2,7 +2,20 @@ library(tidyverse) # CRAN v1.3.0
 library(readxl)
 library(janitor)
 
-# Place of death
+# Daily place of death
+daily_place_of_death <- read_csv(here::here('data','original data', 
+                                      "Figure_7__The_number_of_COVID-19_deaths_in_care_homes_continues_to_increase.csv"),
+                           skip=6)
+# reshape data so it is long
+df_long <- pivot_longer(daily_place_of_death, -Date, names_to = 'location', 
+                        values_to='deaths') %>% 
+  mutate(date=paste0(Date, '-2020'), date=lubridate::dmy(date),
+         location=fct_recode(location, 'Other*'='Other'))
+
+saveRDS(df_long, here::here('data', 'EW_daily_deaths.rds'))
+
+
+# Weekly place of death
 place_of_death <- read_excel(path=here::here('data', 'original data', '2020Mortality.xlsx'), sheet='Covid-19 - Place of occurrence ')  
 
 ONS <- place_of_death %>%
@@ -24,13 +37,20 @@ ONS <- place_of_death %>%
   rename(location=name) %>% 
   mutate(covid_p=covid_19_deaths/total_deaths) %>% 
   mutate(week_ended=as.Date(week_ended, origin = "1899-12-30"), 
+         location_4groups=fct_collapse(location,
+                                       "Home" = "home",
+                                       "Hospital (acute or community, not psychiatric)" = "hospital_acute_or_community_not_psychiatric",
+                                       "Other*" = c("hospice","elsewhere", "other_communal_establishment"),
+                                       "Care home" = "care_home"),
+         
          location=fct_recode(location,
                              "Home" = "home",
                              "Hospital (acute or community, not psychiatric)" = "hospital_acute_or_community_not_psychiatric",
                              "Hospice" = "hospice",
                              "Care home" = "care_home",
                              "Other communal establishment" = "other_communal_establishment",
-                             "Elsewhere" = "elsewhere"))
+                             "Elsewhere" = "elsewhere"), 
+)
 
 
 EW <- ONS %>% 
@@ -39,6 +59,16 @@ EW <- ONS %>%
   group_by(location) %>% 
   fill(firstweek) %>% 
   ungroup() %>% 
-  mutate(proportion_0_base=total_deaths/firstweek*100-100)
+  mutate(proportion_0_base=total_deaths/firstweek*100-100) %>% 
+  group_by(location_4groups) %>% 
+  mutate(firstweek_4groups=ifelse(week_number==11, total_deaths, NA)) %>% 
+  group_by(location_4groups) %>% 
+  fill(firstweek_4groups) %>% 
+  ungroup() %>% 
+  mutate(proportion_0_base_4groups=total_deaths/firstweek_4groups*100-100) %>% 
+  distinct(week_ended, location_4groups, .keep_all = TRUE)
+
 
 saveRDS(EW, here::here('data', 'EW_weekly_deaths.rds'))
+
+
