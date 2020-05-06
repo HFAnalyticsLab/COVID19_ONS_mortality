602d7158ad2ef7bec42182fc149fae1483f49a17
Emma Vestesson <emma.vestesson@gmail.com>
2020-05-06 09:08:09 +0200

    URL now scraped from website

--- src/0_download_data.R
+++ src/0_download_data.R
@@ -1,16 +1,30 @@
+library(curl)
+library(rvest)  
+library(polite)
+
 # Download daily deaths data to recreate ONS chart
 download.file(
-  "https://www.ons.gov.uk/generator?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/bulletins/deathsregisteredweeklyinenglandandwalesprovisional/weekending17april2020/cd850266&format=csv",  destfile = here::here('data','original data', "2020Mortality.xlsx"),
+  "https://www.ons.gov.uk/generator?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/bulletins/deathsregisteredweeklyinenglandandwalesprovisional/weekending24april2020/5db9eecb&format=csv",
   destfile = here::here('data','original data', "Figure_7_The_number_of_COVID_19_deaths_in_care_homes_continues_to_increase.csv"),
   mode = "wb")
 
 
 
-# 2020 - URL needs to be updated weekly
-download.file(
-  "https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fbirthsdeathsandmarriages%2fdeaths%2fdatasets%2fweeklyprovisionalfiguresondeathsregisteredinenglandandwales%2f2020/publishedweek1620201.xlsx",
-  destfile = here::here('data','original data', "2020Mortality.xlsx"),
-  mode = "wb")
+# 2020 - URL scraped from website
+# COVID----
+# Grab links from website
+link <- 'https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/datasets/weeklyprovisionalfiguresondeathsregisteredinenglandandwales'
+bow(link)
+page <- read_html(link)
+links <- page %>% html_nodes(css=".btn--thick") %>% html_attr('href')
+
+file_names <- fs::path_file(links)
+
+link_2020 <- links[str_detect(links, '2020.')]
+
+destfile_ONS <- here::here('data', 'original data', "2020Mortality.xlsx")
+curl_download(paste0('https://www.ons.gov.uk/', link_2020), destfile = destfile_ONS)
+
 
 
 # 2019
